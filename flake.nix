f7fb21277c071e5c6d10a1e7407828932da4ead6
Milner39 <91906877+Milner39@users.noreply.github.com>
2025-12-09 15:15:11 +0000

    chore: init

---
+++ flake.nix
@@ -0,0 +1,29 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+
+    quickshell = {
+      url = "git+https://git.outfoxxed.me/outfoxxed/quickshell";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { self, nixpkgs, flake-utils, quickshell, ... } @ inputs: let
+    inherit (flake-utils.lib) system;
+    systems = [ system.x86_64-linux ];
+
+  in flake-utils.lib.eachSystem systems (system: let
+    pkgs = import nixpkgs { inherit system;
+      config.allowUnfree = true;
+    };
+
+  in {
+    # nix develop
+    devShells.default = pkgs.mkShell {
+      packages = [
+        quickshell.packages.${system}.default.withModules []
+      ];
+    };
+  });
+}
